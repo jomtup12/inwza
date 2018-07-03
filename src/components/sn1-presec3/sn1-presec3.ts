4737ca6c37813b4baa454fff6ce68cf39934647f
sinthuC <sinthu_ch@kkumail.com>
2018-07-03 22:58:43 +0700

    UPDATE presec3

--- src/components/sn1-presec3/sn1-presec3.ts
+++ src/components/sn1-presec3/sn1-presec3.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 import { Events,AlertController } from 'ionic-angular';
-
+import { sn1 } from '../../models/SN1/sn1';
+import { sn1_sub } from '../../models/SN1/sn1_sub';
 /**
  * Generated class for the Sn1Presec3Component component.
  *
@@ -11,18 +12,30 @@ import { Events,AlertController } from 'ionic-angular';
   selector: 'sn1-presec3',
   templateUrl: 'sn1-presec3.html'
 })
-export class Sn1Presec3Component {
-
+export class Sn1Presec3Component  {
+  @Input() sn1:sn1;
   text: string;
   buildingHead: string
   buildingDetail: string
   constructor(private events: Events,private alertCtrl: AlertController) {
-    console.log('Hello Sn1Presec3Component Component');
+    
     this.text = 'Hello World';
     this.buildingHead = 'ครัวเรือนที่ 1';
     this.buildingDetail ='บ้านเลขที่ 199/3';
   }
+  ngOnInit(){
+    if(this.sn1.N!=0 && this.sn1.H3 == null){
+      this.sn1.H3 = [];
+      for(let i=0;i<this.sn1.N;i++){
+        //this.sn1.H3.push(this.sn1_sub)
+        this.addFamily();
+        
+      }
+      
+    }
+  }
 
+  
   presentPrompt() {
     let alert = this.alertCtrl.create({
       title: 'ผลการเข้าพบ',
@@ -68,8 +81,13 @@ export class Sn1Presec3Component {
   
     alert.present();
   }
+  addFamily(){
+    const SN1_sub = new sn1_sub();
+    this.sn1.H3.push(SN1_sub);
+  }
 
-  editBuilding(){
-    this.events.publish("editBuildingSummit");
+  editBuilding(i:number){
+    let index = i;
+    this.events.publish("editBuildingSummit",index);
   }
 }
