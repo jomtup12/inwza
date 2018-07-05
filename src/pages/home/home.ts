0bb8e9918f71fe79570f2a550147cb5ce4cab709
sinthuC <sinthu_ch@kkumail.com>
2018-07-05 23:01:42 +0700

    UPDATE page home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, MenuController, ModalController, LoadingController } from 'ionic-angular';
 import { Sn1Page } from '../sn1/sn1';
+import { sn1 } from '../../models/SN1/sn1';
 
 /**
  * Generated class for the HomePage page.
@@ -19,11 +20,13 @@ export class HomePage {
   a: any
   isFix: boolean
   buttonClicked: boolean = false;
+  SN1:sn1[];
   constructor(public navCtrl: NavController, public navParams: NavParams, public menu: MenuController, private modal: ModalController, public loadingCtrl: LoadingController) {
     menu.enable(true);
     this.n = 2;
     this.a = 1;
     this.isFix = true;
+    this.SN1 = [];
 
 
   }
@@ -33,7 +36,14 @@ export class HomePage {
   }
 
   generateSN1() {
-    this.navCtrl.push(Sn1Page);
+    let sn = new sn1();
+    this.SN1.push(sn);
+    console.log(this.SN1);
+    this.navCtrl.push(Sn1Page,{sn1:this.SN1[this.SN1.length-1]});
+  }
+
+  editBuilding(index:number){
+    this.navCtrl.push(Sn1Page,{sn1:this.SN1[index]})
   }
   openModal() {
     const myModal = this.modal.create('ModalPage');
